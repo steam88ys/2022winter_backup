0d96bd897ad32ed74c0cc344fa2da29df18e0c22
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-03 11:50:23 +0900

    2차시 7번

--- a7.c
+++ a7.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int main () {
-	int a[] = {1, 2, 3, 4, 5};
-	int *a_pt= &a;
-	
-	printf("%d %d\n", a[0], *a_pt); 
-	printf("%d %d\n", a[1], *(a_pt+1)); 
-	printf("%d %d\n", a[2], *(a_pt+2)); 
-	
-//	printf("a주소 = %d\na_pt:%d\n\n", &a, a_pt);
-//	printf("a값:%d\na_pt가 가리키는 값:%d\n",a, *a_pt);
+int solution(int value, char* unit) {
+    int converted = 0;
+    if (unit[0] == 'C')
+        converted = (value * 1.8 )+ 32;
+    if (unit[0] == 'F')
+        converted = (value - 32) / 1.8;
+    return converted;
 }
+
+// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다. main함수는 잘못된 부분이 없으니, solution함수만 수정하세요.
+int main() {
+    int value = 527;
+    char* unit = "C";
+    int ret = solution(value, unit);
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
+
