06fb119be0eae7a879f76be560ad0fa9566e4c63
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-04 14:29:04 +0900

    3차시 7번

--- a7.c
+++ a7.c
@@ -2,22 +2,37 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int value, char* unit) {
-    int converted = 0;
-    if (unit[0] == 'C')
-        converted = (value * 1.8 )+ 32;
-    if (unit[0] == 'F')
-        converted = (value - 32) / 1.8;
-    return converted;
+int solution(int num_apple, int num_carrot, int k) {
+    int answer = 0;
+    if(num_apple < 3 * num_carrot)
+        answer = num_apple / 3;
+    else
+        answer = num_carrot;   
+    num_apple -= 3 * answer;
+    num_carrot -= answer;
+    
+    for(int i = 0; k - (num_apple + num_carrot + i) > 0; i++)
+        if(i % 4 == 0)
+            answer--;
+    return answer;
 }
 
 // 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다. main함수는 잘못된 부분이 없으니, solution함수만 수정하세요.
 int main() {
-    int value = 527;
-    char* unit = "C";
-    int ret = solution(value, unit);
-
+    int num_apple1 = 5;
+    int num_carrot1 = 1;
+    int k1 = 2;
+    int ret1 = solution(num_apple1, num_carrot1, k1);
+    
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
+    
+    int num_apple2 = 10;
+    int num_carrot2 = 5;
+    int k2 = 4;
+    int ret2 = solution(num_apple2, num_carrot2, k2);
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
 }
 
