31e30ebfba533d0afb49038be01c7d41443e5941
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-04 10:45:51 +0900

    3차시 2번

--- a2.c
+++ a2.c
@@ -2,44 +2,58 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int func_a(int arr[], int arr_len){
+int func_a(int current_grade[], int last_grade[], int rank[], int arr_length, int max_diff_grade) {
     int count = 0;
-    for(int i = 0; i < arr_len; ++i)
-        if (arr[i] % 5 == 0)
-            count += 1;
+    for (int i = 0; i < arr_length; i++) {
+        if (current_grade[i] >= 80 && rank[i] <= arr_length / 10)
+            count++;
+        else if (current_grade[i] >= 80 && rank[i] == 1)
+            count++;
+        else if (max_diff_grade == current_grade[i] - last_grade[i])
+            count++;
+    }
     return count;
 }
 
-char* func_b(int three, int five){
-    if (three > five)
-        return "three";
-    else if (three < five)
-        return "five";
-    else
-        return "same";
+int* func_b(int current_grade[], int arr_length) {
+    int *rank = (int*) malloc(sizeof(int) * arr_length);
+    for (int i = 0; i < arr_length; i++) 
+        rank[i] = 1;
+    for (int i = 0; i < arr_length; i++) 
+        for (int j = 0; j < arr_length; j++)
+            if (current_grade[i] < current_grade[j])
+                rank[i]++;
+    return rank;
 }
 
-int func_c(int arr[], int arr_len){
-    int count = 0;
-    for(int i = 0; i < arr_len; ++i)
-        if (arr[i] % 3 == 0)
-            count += 1;
-    return count;
+int func_c(int current_grade[], int last_grade[], int arr_length) {
+    int max_diff_grade = 1;
+    for (int i = 0; i < arr_length; i++) {
+        if (max_diff_grade < current_grade[i] - last_grade[i])
+            max_diff_grade = current_grade[i] - last_grade[i];
+    }
+    return max_diff_grade;
 }
 
-char* solution(int arr[], int arr_len) {
-    int count_three = func_c(arr, arr_len);
-    int count_five = func_a(arr, arr_len);
-    char* answer = func_b(count_three, count_five);
+int solution(int current_grade[], int current_grade_len, int last_grade[], int last_grade_len) {
+    int arr_length = current_grade_len;
+    int* rank = func_b(current_grade, current_grade_len);
+    int max_diff_grade = func_c(current_grade, last_grade, current_grade_len);
+    int answer = func_a(current_grade, last_grade, rank, current_grade_len, max_diff_grade);
     return answer;
 }
 
 // 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
 int main() {
-    int arr[10] = {2, 3, 6, 9, 12, 15, 10, 20, 22, 25};
-    int arr_len = 10;
-    char* ret = solution(arr, arr_len);
+    int current_grade[6] = {70, 100, 70, 80, 50, 95};
+    int current_grade_len = 6;
+    int last_grade[6] = {35, 65, 80, 50, 20, 60};
+    int last_grade_len = 6;
+    int ret = solution(current_grade, current_grade_len, last_grade, last_grade_len);
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %s 입니다.\n", ret);
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
+
+
+
