7e29cf876430ee94bb70f0698497b5d43272ad42
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-04 14:30:00 +0900

    3차시 8번

--- a8.c
+++ a8.c
@@ -2,23 +2,29 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int number) {
-    int count = 0;
-    while(number > 0){
-        int n = number % 10;
-        if (n == 2 || n == 3 || n == 5 || n == 7)
-            count += 1;
-        number /= 10;
-    }
-    return count;
+int solution(int programs[][2], int programs_len) {
+    int answer = 0;    
+    int used_tv[25] = { 0, };
+    
+    for(int i = 0; i < programs_len; i++)
+        for(int j = programs[i][0]; j < programs[i][1]; j++)
+            used_tv[j]++;
+    
+    for(int i = 0; i < 25; i++)
+        if(used_tv[i] >= 2)
+            answer++;
+
+    return answer;
 }
 
 // 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다. main함수는 잘못된 부분이 없으니, solution함수만 수정하세요.
 int main() {
-    int number = 29022531;
-    int ret = solution(number);
-
+    int programs[3][2] = {{1, 6}, {3, 5}, {2, 8}};
+    int programs_len = 3;
+    int ret = solution(programs, programs_len);
+    
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
+
 
