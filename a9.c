49e9058687a2d7db7202a1315b47aef86b05315a
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-04 14:31:01 +0900

    3차시 9번

--- a9.c
+++ a9.c
@@ -2,27 +2,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int votes[], int votes_len, int N, int K) {
-    int counter[11] = {0};
-    for(int i = 0; i < votes_len; ++i)
-        counter[votes[i]] += 1;
-    int answer = 0;
-    for(int i = 0; i <= N; ++i)
-        if(counter[i] == K)
-            answer += 1;
-    return answer;
+int solution(int day, int numbers[], int numbers_len) {
+    int count = 0;
+    for(int i = 0; i < numbers_len; i++)
+    	if(numbers[i]%2 == day%2)
+    		count++;
+    return count;
 }
 
 // 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다. main함수는 잘못된 부분이 없으니, solution함수만 수정하세요.
 int main() {
-    int votes[10] = {2, 5, 3, 4, 1, 5, 1, 5, 5, 3};
-    int votes_len = 10;
-    int N = 5;
-    int K = 2;
-    int ret = solution(votes, votes_len, N, K);
-
+    int day = 17;
+    int numbers[] = {3285, 1724, 4438, 2988, 3131, 2998};
+    int numbers_len = 6;
+    int ret = solution(day, numbers, numbers_len);
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
-
 
