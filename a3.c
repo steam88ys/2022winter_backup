bf5163420bef71a5ddc74e838b1d662f4b2e7a6a
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-04 11:07:57 +0900

    3차시 3번

--- a3.c
+++ a3.c
@@ -3,26 +3,35 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int N, int M) {
+int solution(int scores[], int scores_len) {
     // 여기에 코드를 작성해주세요.
-    int answer = 0;
+    int answer = 0, sum=0;
+    int max = scores[0], min = scores[0];
     
-    for(int i=N; i<=M; i++) {
-    	if(i%2==0) answer+= (i*i);
+    for(int i=0; i<scores_len; i++) {
+    	if(scores[i]>max) max = scores[i];
+    	if(scores[i]<min) min = scores[i];
+    	sum+=scores[i];
 	}
-    
+	
+	answer = (sum-max-min)/(scores_len-2);
     return answer;
 }
 
 // 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
 int main() {
-    int N = 4;
-    int M = 7;
-    int ret = solution(N, M);
-
+    int scores1[10] = {35, 28, 98, 34, 20, 50, 85, 74, 71, 7};
+    int scores1_len = 10;
+    int ret1 = solution(scores1, scores1_len);
+    
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
+    
+    int scores2[5] = {1, 1, 1, 1, 1};
+    int scores2_len = 5;
+    int ret2 = solution(scores2, scores2_len);
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
 }
-
-
 
