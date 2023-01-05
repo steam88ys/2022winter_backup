d87b5d96b698fe781c07d2375504aab65295aa0e
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-05 10:38:30 +0900

    4차시 6번

--- a6.c
+++ a6.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 
-char* solution(int tile_length) {
-    char* answer = (char*) malloc(sizeof(char) * (tile_length+1) );
-    char com[6] = {'R','R','R','G','G','B'};
-    if(tile_length%6 == 1 || tile_length%6 == 2 || tile_length%6 == 4)
-        strcpy(answer, "-1");
-    else{
-        for(int i = 0; i < tile_length; i++)
-            answer[i] = com[i%6];
-        answer[tile_length]='\0';
-    }
-    return answer;
+int solution(int point) {
+    if (point < 1000)
+        return 0;
+    return point / 100 * 100;
 }
 
-/// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
+// 아래는 테스트케이스 출력을 해보기 위한 코드입니다. 아래에는 잘못된 부분이 없으니, 위의 코드만 수정하세요.
 int main() {
-    int tile_length1 = 11;
-    char* ret1 = solution(tile_length1);
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %s 입니다.\n", ret1);
+    int point = 2323;
+    int ret = solution(point);
     
-    int tile_length2 = 16;
-    char* ret2 = solution(tile_length2);
-
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %s 입니다.\n", ret2);
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
 
