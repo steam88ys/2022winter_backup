db5090148db19894f1d939bb456790c1c65af3ab
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-05 10:38:21 +0900

    4차시 5번

--- a5.c
+++ a5.c
@@ -1,56 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 
-int solution(int member_age[], int member_age_len, char* transportation) {
-	int adult_expense = 0;
-	int child_expense = 0;
-
-	if(!strcmp(transportation, "Bus")) {
-		adult_expense = 40000;
-		child_expense = 15000;
-	}
-	else if(!strcmp(transportation, "Ship")) {
-		adult_expense = 30000;
-		child_expense = 13000;
-	}
-	else if(!strcmp(transportation, "Airplane")) {
-		adult_expense = 70000;
-		child_expense = 45000;
-	}
-
-	if(member_age_len >= 10) {
-		adult_expense = adult_expense*0.9;
-		child_expense = child_expense*0.8;
-	}
-	int total_expenses = 0;
-	for(int i = 0; i < member_age_len; i++) {
-		if(member_age[i]>=20)
-			total_expenses += adult_expense;
-		else
-			total_expenses += child_expense;
-	}
-
-	return total_expenses;
+int solution(int calorie[], int calorie_len) {
+    int min_cal = calorie[0];
+    int answer = 0;
+    for(int i=0; i<calorie_len; i++) {
+        if(calorie[i] > min_cal)
+            answer += calorie[i] - min_cal;
+        else
+            min_cal = calorie[i];
+    }
+    return answer;
 }
 
-// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
+// 아래는 테스트케이스 출력을 해보기 위한 코드입니다. 아래에는 잘못된 부분이 없으니, 위의 코드만 수정하세요.
 int main() {
-    int member_age1[5] = {13, 33, 45, 11, 20};
-    int member_age1_len = 5;
-    char* transportations1 = "Bus";
-    int ret1 = solution(member_age1, member_age1_len, transportations1);
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
+    int calorie[] = {713, 665, 873, 500, 751};
+    int ret = solution(calorie, 5);
     
-    int member_age2[10] = {25, 11, 27, 56, 7, 19, 52, 31, 77, 8};
-    int member_age2_len = 10;
-    char* transportations2 = "Ship";
-    int ret2 = solution(member_age2, member_age2_len, transportations2);
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
-
 
 
