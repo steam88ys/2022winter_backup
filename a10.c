fc870f45dba39f4b810f24f3554fad2278cb1b36
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-05 13:06:57 +0900

    4차시 10번

--- a10.c
+++ a10.c
@@ -1,24 +1,26 @@
+// 다음과 같이 include를 사용할 수 있습니다.
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int arr[], int arr_len) {
+int solution(int scores[], int scores_len, int cutline) {
+    // 여기에 코드를 작성해주세요.
     int answer = 0;
-    for (int i = 0; i < arr_len; i++) 
-        for (int j = 0; j < arr_len; j++) 
-            if (arr[i]%2 == 0 && arr[i]/2 == arr[j]) {
-                answer++;
-                break;
-            }
+    
+    for(int i=0; i<scores_len; i++) {
+    	if(scores[i]>=cutline) answer++; 
+	}
+    
     return answer;
 }
 
-// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다. main함수는 잘못된 부분이 없으니, solution함수만 수정하세요.
+// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
 int main() {
-    int arr[] = {4, 8, 3, 6, 7};
-    int arr_len = 5;
-    int ret = solution(arr, arr_len);
-
+    int scores[5] = {80, 90, 55, 60, 59};
+    int scores_len = 5;
+    int cutline = 60;
+    int ret = solution(scores, scores_len, cutline);
+    
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
