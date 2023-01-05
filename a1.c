95af2979d7d7eb75d4430300abdb08b09d17d808
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2023-01-05 09:09:52 +0900

    4차시 1번

--- a1.c
+++ a1.c
@@ -2,40 +2,31 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int compare(const void *a, const void *b){
-    return ( *(int*)b - *(int*)a );
-}
+int* solution(char* schedule[], int schedule_len) {
+    int x_count = 0;
+    for(int i=0; i<10; i++)
+        if(schedule[i][0] == 'X')
+             x_count++;
 
-int func_a(int scores[], int scores_len, int score){
-    for(int rank = 0; rank < scores_len; rank++)
-        if(scores[rank] == score)
-            return rank + 1;
-    return 0;
-}
-
-void func_b(int arr[], int arr_len){
-    qsort(arr, arr_len, sizeof(int), compare);
-}
-
-int func_c(int arr[], int n){
-    return arr[n];
-}
-
-int solution(int scores[], int scores_len, int n) {
-    int score = func_c(scores, n);
-    func_b(scores, scores_len);
-    int answer = func_a(scores, scores_len, score);
+    int* answer = (int*)malloc(sizeof(int)*x_count);
+    for(int i=0, j=0; i<10; i++) {
+        if(schedule[i][0] == 'X') {
+             answer[j++] = i+1;
+        }
+    }
     return answer;
 }
 
 // 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
 int main() {
-    int scores[4] ={20, 60, 98, 59};
-    int scores_len = 4;
-    int n = 3;
-    int ret = solution(scores, scores_len, n);
+    char* schedule[] = {"O", "X", "X", "O", "O", "O", "X", "O", "X", "X"};
+    int* ret = solution(schedule, 10);
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+    printf("solution 함수의 반환 값은 ");
+    for(int i=0; i<5; i++)
+        printf("%d, ", ret[i]);
+    printf(" 입니다.");
+
 }
 
